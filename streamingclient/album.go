7a10a40abae7f7f5775b36eebb3b05dd3b92634f
Anton Satin <astair36@gmail.com>
2017-01-14 02:37:12 +0300

    global search basic

--- streamingclient/album.go
+++ streamingclient/album.go
@@ -1,6 +1,32 @@
 package streamingclient
 
+import "sync"
+
 type Album struct {
 	ArtistName string
 	Name       string
+}
+
+type AlbumFinder interface {
+	FindDeezer() string
+	FindYandex() string
+	FindLinks() chan string
+}
+
+func (album Album) FindLinks() chan string {
+	links := make(chan string)
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	go addLink(&links, album.FindDeezer(), &wg)
+	go addLink(&links, album.FindYandex(), &wg)
+	wg.Wait()
+	return links
+}
+
+func addLink(links *chan string, link string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	if link != "" {
+		*links <- link
+	}
 }
---
+++ streamingclient/album_test.go
@@ -0,0 +1,1 @@
+package streamingclient_test
